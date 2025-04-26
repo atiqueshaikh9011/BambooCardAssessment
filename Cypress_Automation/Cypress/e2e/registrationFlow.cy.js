3c93ec9d820a28bf563aa7210c72da9070de0009
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:48:57 -0400

    Update registrationFlow.cy.js

--- Cypress_Automation/Cypress/e2e/registrationFlow.cy.js
+++ Cypress_Automation/Cypress/e2e/registrationFlow.cy.js
@@ -1,1 +1,25 @@
+import RegisterPage from '../pages/RegisterPage';
+import LoginPage from '../pages/LoginPage';
+
+describe('Registration and Login Validation', () => {
+  beforeEach(() => {
+    cy.fixture('userData').then(function(data) {
+      this.data = data;
+    });
+  });
+
+  it('Register a new user and validate login', function() {
+    RegisterPage.navigate();
+    RegisterPage.fillRegistrationForm(this.data.firstName, this.data.lastName, this.data.email, this.data.password);
+    RegisterPage.submit();
+
+    cy.contains('Thank you for registering').should('be.visible');
+
+    cy.get('.account > .action').click(); // logout
+    LoginPage.navigate();
+    LoginPage.login(this.data.email, this.data.password);
+
+    cy.contains('Welcome, ' + this.data.firstName).should('be.visible');
+  });
+});
 
