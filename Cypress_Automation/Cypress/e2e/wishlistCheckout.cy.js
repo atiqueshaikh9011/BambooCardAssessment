718dfc414fd1c3c5c504ff6dd0aa25f07374aa57
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:49:59 -0400

    Update wishlistCheckout.cy.js

--- Cypress_Automation/Cypress/e2e/wishlistCheckout.cy.js
+++ Cypress_Automation/Cypress/e2e/wishlistCheckout.cy.js
@@ -1,1 +1,14 @@
+import WishlistPage from '../pages/WishlistPage';
+
+describe('Add to Wishlist and Checkout from Wishlist', () => {
+  it('Add product to wishlist and checkout', () => {
+    cy.visit('/');
+
+    WishlistPage.addToWishlist();
+    WishlistPage.goToWishlist();
+    WishlistPage.addToCartFromWishlist();
+
+    cy.contains('Proceed to Checkout').should('be.visible');
+  });
+});
 
