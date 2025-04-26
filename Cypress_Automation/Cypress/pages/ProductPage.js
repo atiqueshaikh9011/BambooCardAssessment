c2c52d414f37c2b5ee4f14f29b2025976cbf0835
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:47:36 -0400

    Update ProductPage.js

--- Cypress_Automation/Cypress/pages/ProductPage.js
+++ Cypress_Automation/Cypress/pages/ProductPage.js
@@ -1,1 +1,17 @@
+class ProductPage {
+  addMultipleProductsToCart() {
+    cy.get('.product-item').first().click();
+    cy.get('button[title="Add to Cart"]').click();
+    cy.go('back');
+    cy.get('.product-item').eq(1).click();
+    cy.get('button[title="Add to Cart"]').click();
+  }
+
+  goToCart() {
+    cy.get('.showcart').click();
+    cy.contains('View and Edit Cart').click();
+  }
+}
+
+export default new ProductPage();
 
