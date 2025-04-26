bc7c35df86b5c4a56124fc74df9ea117ce7abe96
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:47:59 -0400

    Update WishListPage.js

--- Cypress_Automation/Cypress/pages/WishListPage.js
+++ Cypress_Automation/Cypress/pages/WishListPage.js
@@ -1,1 +1,16 @@
+class WishlistPage {
+  addToWishlist() {
+    cy.get('.product-item').first().trigger('mouseover');
+    cy.get('[data-action="add-to-wishlist"]').first().click();
+  }
 
+  goToWishlist() {
+    cy.get('.wishlist.block').click();
+  }
+
+  addToCartFromWishlist() {
+    cy.get('.tocart').click();
+  }
+}
+
+export default new WishlistPage();
