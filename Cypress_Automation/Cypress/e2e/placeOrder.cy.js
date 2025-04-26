3f6b29b482b5a88bbd21f14134f696732497153f
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:49:22 -0400

    Update placeOrder.cy.js

--- Cypress_Automation/Cypress/e2e/placeOrder.cy.js
+++ Cypress_Automation/Cypress/e2e/placeOrder.cy.js
@@ -1,1 +1,17 @@
+import ProductPage from '../pages/ProductPage';
+
+describe('Place Order with Multiple Products', () => {
+  it('Add products to cart and validate price calculation', () => {
+    cy.visit('/');
+
+    ProductPage.addMultipleProductsToCart();
+    ProductPage.goToCart();
+
+    cy.get('.cart-totals').within(() => {
+      cy.get('.grand.totals > .price').should('exist');
+    });
+
+    cy.contains('Proceed to Checkout').should('be.visible');
+  });
+});
 
