04bd55413a0227fb6c97d112886be74592eeb6fb
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:46:52 -0400

    Update LoginPage.js

--- Cypress_Automation/Cypress/pages/LoginPage.js
+++ Cypress_Automation/Cypress/pages/LoginPage.js
@@ -1,1 +1,14 @@
+class LoginPage {
+  navigate() {
+    cy.visit('/customer/account/login/');
+  }
+
+  login(email, password) {
+    cy.get('#email').type(email);
+    cy.get('#pass').type(password);
+    cy.get('button[title="Sign In"]').click();
+  }
+}
+
+export default new LoginPage();
 
