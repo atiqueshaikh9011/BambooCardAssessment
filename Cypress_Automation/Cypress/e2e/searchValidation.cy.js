b51dad2077f55928b07d72db6ff28d30def4173f
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:50:25 -0400

    Update searchValidation.cy.js

--- Cypress_Automation/Cypress/e2e/searchValidation.cy.js
+++ Cypress_Automation/Cypress/e2e/searchValidation.cy.js
@@ -1,1 +1,14 @@
+describe('Search and Validate Results', () => {
+  beforeEach(() => {
+    cy.fixture('userData').then(function(data) {
+      this.data = data;
+    });
+  });
+
+  it('Search for a product and validate search results', function() {
+    cy.visit('/');
+    cy.get('#search').type(this.data.searchItem + '{enter}');
+    cy.get('.search.results').should('contain', this.data.searchItem);
+  });
+});
 
