c12f3558501087874ce70e27d8c6c9769aae660c
atiqueshaikh9011 <44003016+atiqueshaikh9011@users.noreply.github.com>
2025-04-26 09:46:28 -0400

    Update RegisterPage.js

--- Cypress_Automation/Cypress/pages/RegisterPage.js
+++ Cypress_Automation/Cypress/pages/RegisterPage.js
@@ -1,1 +1,19 @@
+class RegisterPage {
+  navigate() {
+    cy.visit('/customer/account/create/');
+  }
 
+  fillRegistrationForm(firstName, lastName, email, password) {
+    cy.get('#firstname').type(firstName);
+    cy.get('#lastname').type(lastName);
+    cy.get('#email_address').type(email);
+    cy.get('#password').type(password);
+    cy.get('#password-confirmation').type(password);
+  }
+
+  submit() {
+    cy.get('button[title="Create an Account"]').click();
+  }
+}
+
+export default new RegisterPage();
